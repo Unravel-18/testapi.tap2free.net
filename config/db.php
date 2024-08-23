c5d6aadeee4e130737906adc35316cf1104eb90c
Ilya <barich2002@gmail.com>
2024-08-23 20:29:13 +0200

    commit

--- config/db.php
+++ config/db.php
@@ -2,44 +2,44 @@
 
 return [
     "mysql" => [// По умолчанию
-        "host" => "127.0.0.1",
-        "port" => "3306",
-        "database" => "testapi",
-        "username" => "testapi",
-        "password" => "uI8eR1pK6q",
+        "host" => env('DB_HOST'),
+        "port" => env('DB_PORT'),
+        "database" => env('DB_DATABASE'),
+        "username" => env('DB_USERNAME'),
+        "password" => env('DB_PASSWORD'),
         
         "ip_save" => true, // Для сохранения IP при контроле активации токена
         "badip_save" => true, // Для сохранения IP в badip при активации токена
     ],
     "mysql_prokeys" => [// Для хранения ключей pro
-        "host" => "127.0.0.1",
-        "port" => "3306",
-        "database" => "testapi",
-        "username" => "testapi",
-        "password" => "uI8eR1pK6q",
+        "host" => env('DB_HOST'),
+        "port" => env('DB_PORT'),
+        "database" => env('DB_DATABASE'),
+        "username" => env('DB_USERNAME'),
+        "password" => env('DB_PASSWORD'),
     ],
     "mysql_google_tokens" => [// Для хранения гугл токена
-        "host" => "127.0.0.1",
-        "port" => "3306",
-        "database" => "testapitoken",
-        "username" => "testapitoken",
-        "password" => "zJ1oY0nE4v",
+        "host" => env('DB_HOST_2'),
+        "port" => env('DB_PORT_2'),
+        "database" => env('DB_DATABASE_2'),
+        "username" => env('DB_USERNAME_2'),
+        "password" => env('DB_PASSWORD_2'),
     ],
     "mysql_keys" => [// Для хранения ключей
-        "host" => "127.0.0.1",
-        "port" => "3306",
-        "database" => "testapi",
-        "username" => "testapi",
-        "password" => "uI8eR1pK6q",
+        "host" => env('DB_HOST'),
+        "port" => env('DB_PORT'),
+        "database" => env('DB_DATABASE'),
+        "username" => env('DB_USERNAME'),
+        "password" => env('DB_PASSWORD'),
         
         "token_save" => true, // Для сохранения данных токена
     ],
     "mysql_3" => [
-        "host" => "127.0.0.1",
-        "port" => "3306",
-        "database" => "testapi",
-        "username" => "testapi",
-        "password" => "uI8eR1pK6q",
+        "host" => env('DB_HOST'),
+        "port" => env('DB_PORT'),
+        "database" => env('DB_DATABASE'),
+        "username" => env('DB_USERNAME'),
+        "password" => env('DB_PASSWORD'),
         
         "badip_save" => true, // Для сохранения IP в badip при активации токена
         "ip_save" => true, // Для сохранения IP при контроле активации токена
